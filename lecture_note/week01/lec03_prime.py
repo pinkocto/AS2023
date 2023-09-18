bcec64c57c48e591ae9e4bf5d68c9a183801e083
pinkocto <pucca4881@gmail.com>
2023-09-18 10:25:39 +0900

    .

---
+++ lecture_note/week01/lec03_prime.py
@@ -0,0 +1,15 @@
+def is_prime(num):
+    pass
+
+
+def main():
+    num = 3
+
+    if is_prime(num):
+        print(f"{num}은/는 소수입니다.")
+    else:
+        print(f"{num}은/는 소수가 아닙니다.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ lecture_note/week01/lec05_factorial.py
@@ -0,0 +1,10 @@
+def fact(num):
+    pass
+
+
+def main():
+    print(f"10!은 {fact(10)}입니다.")
+
+
+if __name__ == "__main__":
+    main()

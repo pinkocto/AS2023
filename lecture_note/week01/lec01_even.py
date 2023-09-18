80491c6eb9eb2e355821a09b111fcd2bb5b01311
pinkocto <pucca4881@gmail.com>
2023-09-18 10:16:25 +0900

    .

---
+++ lecture_note/week01/lec01_even.py
@@ -0,0 +1,11 @@
+def main():
+    # a = int(input("숫자를 입력하세요."))
+    a = 3
+    if a % 2 == 0:
+        print(f"{a}은/는 짝수입니다.")
+    else:
+        print(f"{a}은/는 홀수입니다.")
+
+
+if __name__ == "__main__":
+    main()

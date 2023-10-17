6234401149655ed830beb0bb48c26c6880bcec74
pinkocto <pucca4881@gmail.com>
2023-10-18 03:30:38 +0900

    week04-2 update

--- week04-2/app.py
+++ week04-2/app.py
@@ -4,21 +4,23 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def first_page():
     return render_template("index.html")
 
 @app.route("/about")
-def about():
+def about_me_func():
     return render_template("about_me.html")
 
 @app.route("/blog_list")
 def blog_list_func():
     datasets = [
-        {"title":"제목1", "content":"제목1의 내용"},
+        {"title": "제목1", "content": "제목1의 내용"},
         {"title": "제목2", "content": "제목2의 내용"},
         {"title": "제목3", "content": "제목3의 내용"},
         {"title": "제목4", "content": "제목4의 내용"}
     ]
-    return render_template("blog_list.html", posts = datasets)
+    # return render_template("blog_list.html", posts = datasets)
+    return render_template("blog_list.html", posts=datasets[::-1])
 
-app.run(host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
